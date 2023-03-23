63b2fd847760dd0d1f2f381d1c5c7f38bcdce2a6
will1415 <114450715+will1415@users.noreply.github.com>
2023-03-22 22:09:43 -0400

    Add FastAPI Endpoints

--- backend/station.py
+++ backend/station.py
@@ -6,9 +6,7 @@ import random
 import numpy as np
 import pickle
 import pika
-
-app = FastAPI()
-
+import pprint
 
 class Vehicle_Type(Enum):
     ambulance = 1
@@ -22,10 +20,17 @@ class Tile_Type(Enum):
     Free = 1
     Blocked = 2 ##Not sure if we need this
 
+current_path = []
+map = [[1, 1, 1, 0, 1], [1, 1, 0, 0, 1], [0, 1, 0, 1, 1], [1, 1, 0, 0, 0],  [1, 1, 1, 1, 1]]
+stations = []
+
+# Dispatch notify all stations (level + coordinates)
+# Dispatch choose based on the response and list (shortest distance) and dispatch sends a
+# message designating which stations to send
 class Station:
     def __init__(self, number, vehicles, coordinates):
         self.number = number
-        self.serial = vehicles
+        self.vehicles = vehicles
         self.coordinates = coordinates
 
 class Vehicle:
@@ -47,11 +52,9 @@ def generate_vehicles(number_of_vehicles, station_coords):
 def build_stations(number_of_stations, coordinates, station_num):
     stations = []
     for i in range(number_of_stations):
-        station = Station(station_num[i], generate_vehicles(5, coordinates[i]))
+        station = Station(station_num[i], generate_vehicles(3, coordinates[i]), coordinates[i])
         stations.append(station)
     return stations
-
-
 
 ##Generate Vehicle Path
 def generate_path(maze, source, destination, visited, path, paths, rows, columns):
@@ -76,27 +79,15 @@ def generate_path(maze, source, destination, visited, path, paths, rows, columns
   visited[x][y] = False
   return paths
 
-def get_path(maze, source, destination, rows, columns):
+def get_paths(maze, source, destination):
+  rows = len(maze)
+  columns = len(maze[0])
   visited = [[False]*rows for _ in range(columns)] #may be inversed
   path = [source]
   paths = []
-  return generate_path(maze, source, destination, visited, path, paths, rows, columns)
-
-##Testing
-def test_path():
-    map = [[1, 1, 1, 0, 1], [1, 1, 0, 0, 1], [0, 1, 0, 1, 1], [1, 1, 0, 0, 0],  [1, 1, 1, 1, 1]]
-    rows = len(map)
-    columns = len(map[0])
-    print(rows)
-    print(columns)
-    print(np.matrix(map))
-    start = (0,0)
-    end = (4,4)
-    path = get_path(map, start, end, rows, columns)
-    print("Path is: ")
-    print(path)
-
-test_path()
+  path = generate_path(maze, source, destination, visited, path, paths, rows, columns)
+  current_path.append(path)
+  return path
 
 ##Receive Dispatch Request
 def handle_dispatch_request():
@@ -126,9 +117,73 @@ def send_dispatch_response(station_num, status):
     channel.queue_declare(queue='Station-Response')
     channel.basic_publish(exchange='', routing_key='Station-Response', body=pickle.dumps(station_response))
     connection.close()
-    
+
+##Testing
+def test_path():
+    map = [[1, 1, 1, 0, 1], [1, 1, 0, 0, 1], [0, 1, 0, 1, 1], [1, 1, 0, 0, 0],  [1, 1, 1, 1, 1]]
+    print(np.matrix(map))
+    start = (0,0)
+    end = (4,4)
+    path = get_paths(map, start, end)
+    print("Path is: ")
+    print(path)
+
+##Testing
+def create_get_test_path():
+    start = (0,0)
+    end = (4,4)
+    path = get_paths(map, start, end)
+    return path
 
 
-if __name__ == "__station__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    #handle_dispatch_request()
+# Initializes Stations
+def init_stations():
+    station_coords = []
+    station1 = (3,5)
+    station2 = (6,2)
+    station3 = (9,9)
+    station4 = (8,1)
+    station_coords.append(station1)
+    station_coords.append(station2)
+    station_coords.append(station3)
+    station_coords.append(station4)
+
+    station_number = []
+    station1_num = 1234
+    station2_num = 3612
+    station3_num = 7624
+    station4_num = 5135
+    station_number.append(station1_num)
+    station_number.append(station2_num)
+    station_number.append(station3_num)
+    station_number.append(station4_num)
+    return build_stations(4, station_coords, station_number)
+
+
+
+test_path()
+
+def get_path():
+    return current_path.pop(0)
+
+def get_stations():
+    return stations
+
+def get_station_vehicles(station_id):
+    for station in stations:
+        if (station_id == station.number):
+            return station
+    return "Does not exist"
+
+#Displaying Initial Stations
+for station in stations:
+    print("Station: ")
+    print(station.number)
+    print(station.coordinates)
+    print("Vehicles: ")
+    for vehicle in station.vehicles:
+        print(vehicle.id)
+        print(vehicle.vehicle)
+        print(vehicle.coordinates)
+        print(vehicle.available)
+    print("------------")
